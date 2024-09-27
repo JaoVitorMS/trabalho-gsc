45663ed478b507c105fd30068a6ae2415af0afce
João Vitor <joao.vitorms1509@gmail.com>
2024-09-27 14:55:38 -0300

    hotFix

--- src/main/java/br/pucrs/edu/ecommerce/usuarios/Usuario.java
+++ src/main/java/br/pucrs/edu/ecommerce/usuarios/Usuario.java
@@ -1,8 +1,5 @@
 package br.pucrs.edu.ecommerce.usuarios;
 
 public interface Usuario<T> {
-    void adicionar(T obj);
-    void remover(T obj);
-    void atualizar(T obj);
-    T buscarPorId(int id);
+
 }
