9646b915a0f2584e168d848e4aee1f800f52a551
João Vitor <joao.vitorms1509@gmail.com>
2024-09-30 20:13:37 -0300

    admin relatory method, 50%

--- src/main/java/br/pucrs/edu/ecommerce/usuarios/Administrador.java
+++ src/main/java/br/pucrs/edu/ecommerce/usuarios/Administrador.java
@@ -2,10 +2,11 @@ package br.pucrs.edu.ecommerce.usuarios;
 
 import br.pucrs.edu.ecommerce.pedidos.Pedido;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Administrador extends Usuario {
-
 
     public Administrador ( int id, String nome, String func ) {
         super(id, nome, func);
@@ -16,16 +17,23 @@ public class Administrador extends Usuario {
     Somente o administrador pode aprovar ou reprovar pedidos.
     Implementar a funcionalidade de visualização e avaliação de pedidos.
      */
+    List<Pedido> listaTodosPedidos = new ArrayList<>();
     public void aprovarPedidos(){}
+    public void reprovarPedidos(){}
 
     public List<Pedido> getPedidosPendentes(){return null;}
 
-    public List<Pedido> listarPedidos (int dataInicial, int dataFinal){return null;}
+    public List<Pedido> listarPedidos (int dataPrimaria, int dataSecundaria){return null;}
 
+    private void pedidos30dias(){
+        List<Pedido> lista30dias = listaTodosPedidos.stream()
+                                                    .filter(dia -> /*getDia()*/ <= 30)
+                                                    .toList();
 
+        System.out.println("Pedidos feitos nos ultimos 30 dias: "+lista30dias);
+    }
 
-
-    public List<String> relatorio(){
+    public void gerarRelatorio(){
     /*
     Número total de pedidos (aprovados e reprovados com percentuais).
     Pedidos feitos nos últimos 30 dias e o valor médio.
@@ -34,8 +42,16 @@ public class Administrador extends Usuario {
 
      */
 
-        return null;
+        int num = listaTodosPedidos.size();
+        System.out.println("O numero de pedidos no total é: "+num);
 
+        pedidos30dias();
+
+        //valor total departamento
+
+
+        //logica para isso
+        getPedidosPendentes();
 
     }
 
