7197ba83fc6ef50c90984ed15056d18af57b7833
guigs028 <guidentzien10@gmail.com>
2024-10-01 15:47:55 -0300

    Classe Produto finalizada

--- src/main/java/br/pucrs/edu/ecommerce/produtos/Produto.java
+++ src/main/java/br/pucrs/edu/ecommerce/produtos/Produto.java
@@ -1,4 +1,48 @@
 package br.pucrs.edu.ecommerce.produtos;
 
 public class Produto {
+    //atributos
+    private String descricao;
+    private double valorUnitario;
+    private int quantidade;
+    private double valorTotal;
+    
+    //construtor
+    public Produto(String descricao, double valorUnitario, int quantidade) {
+        this.descricao = descricao;
+        this.valorUnitario = valorUnitario;
+        this.quantidade = quantidade;
+        this.valorTotal = calculaValorTotal();
+    }
+
+    //método para calcular o valor total do produto
+    private double calculaValorTotal() {
+        return valorUnitario * quantidade;
+    }
+
+    //getters e setters
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public double getValorTotal() {
+        return valorTotal;
+    }
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 }
