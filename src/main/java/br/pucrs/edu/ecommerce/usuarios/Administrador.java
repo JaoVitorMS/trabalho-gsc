c63d71bc922be3d98b2b6732f93eec6272998124
João Vitor <joao.vitorms1509@gmail.com>
2024-10-06 21:44:01 -0300

    Novos métodos

--- src/main/java/br/pucrs/edu/ecommerce/usuarios/Administrador.java
+++ src/main/java/br/pucrs/edu/ecommerce/usuarios/Administrador.java
@@ -1,26 +1,55 @@
 package br.pucrs.edu.ecommerce.usuarios;
 
 import br.pucrs.edu.ecommerce.pedidos.Pedido;
+import br.pucrs.edu.ecommerce.pedidos.STATUS;
+import br.pucrs.edu.ecommerce.produtos.Produto;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Administrador extends Usuario {
-
 
     public Administrador ( int id, String nome, String func ) {
         super(id, nome, func);
     }
 
+    STATUS pendente = STATUS.PENDENTE;
+    STATUS aprovado = STATUS.APROVADO;
+    STATUS reprovado = STATUS.REPROVADO;
 
     /*
     Somente o administrador pode aprovar ou reprovar pedidos.
     Implementar a funcionalidade de visualização e avaliação de pedidos.
      */
-    public void aprovarPedidos(){}
+    private List<Pedido> getPedidosPendentes(List<Pedido> pedidos){
+        List<Pedido> pedidosPendentes = new ArrayList<>();
 
-    public List<Pedido> getPedidosPendentes(){return null;}
+        for (Pedido pedido : pedidos) {
+            if (pedido.getStatus() == pendente) {
+                pedidosPendentes.add(pedido);
+            }
+        }
+        return pedidosPendentes;
+    }
 
-    public List<Pedido> listarPedidos (int dataInicial, int dataFinal){return null;}
+
+
+    public boolean aprovarPedidos(String desc, List<Pedido> pedidos){
+        List<Pedido> pedidosPendentes = getPedidosPendentes(pedidos);
+        for (Pedido pedido : pedidosPendentes) {
+            for(Produto p : pedido.getProdutos()){
+                if(p.getDescricao().equals(desc)){
+                    pedido.setStatus(aprovado);
+                    return true;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public List<Pedido> listarPedidosEntreDatas (Date dataInicial, Date dataFinal){return null;}
 
 
 
